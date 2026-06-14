a252b43736d150ef696fe923ec6cc7817d78e4d2
zi3783 <3573183772@qq.com>
2026-06-14 17:58:42 +0800

    优化访问次数从本地缓存写入mysql的方式

--- src/main/java/io/github/zi3783/mapper/ShortUrlMapper.java
+++ src/main/java/io/github/zi3783/mapper/ShortUrlMapper.java
@@ -1,9 +1,9 @@
 package io.github.zi3783.mapper;
 
-import cn.hutool.core.lang.Snowflake;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import io.github.zi3783.entity.ShortUrlMap;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
@@ -25,4 +25,6 @@ public interface ShortUrlMapper extends BaseMapper<ShortUrlMap> {
      */
     @Select("select id from short_url_map where is_deleted = 0")
     List<Long> getIdAndTargetUrl();
+
+    void UpdateBatchVisitedCountFromId(@Param("list") List<Long> list, @Param("ids") List<Long> ids);
 }
--- src/main/java/io/github/zi3783/util/FreshMysql.java
+++ src/main/java/io/github/zi3783/util/FreshMysql.java
@@ -2,8 +2,10 @@ package io.github.zi3783.util;
 
 import io.github.zi3783.entity.ShortUrlMap;
 import io.github.zi3783.mapper.ShortUrlMapper;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -22,16 +24,36 @@ public class FreshMysql{
     @Autowired
     private ShortUrlMapper shortUrlMapper;
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelay = 20000)
     public void FreshCounter() {
         log.info("FreshCounter start");
-        List<ShortUrlMap> list = new ArrayList<>();
+        List<Long> list = new ArrayList<>();
+        List<Long> ids = new ArrayList<>();
         for(Map.Entry<String,LongAdder> entry:counterMapper.entrySet()){
             Long id = Long.valueOf(entry.getKey());
             LongAdder counter = entry.getValue();
             Long sum = counter.sumThenReset();
             if(sum <= 0) continue;
-            shortUrlMapper.incrementVisitedCount(id,sum);
+//            try {
+//                shortUrlMapper.incrementVisitedCount(id, sum);
+//            }catch (Exception e){
+//                log.warn("批量更新失败,id:{},sum:{}",id,sum,e);
+//                //回滚
+//                counter.add(sum);
+//            }
+            list.add(sum);
+            ids.add(id);
+        }
+        if(list.isEmpty()) return;
+        try{
+            shortUrlMapper.UpdateBatchVisitedCountFromId(list, ids);
+        }catch (Exception e){
+            log.warn("写入mysql错误,回滚,warn:",e);
+            int i = 0;
+            for(Long id:ids){
+                LongAdder longAdder = counterMapper.get(id.toString());
+                longAdder.add(list.get(i++));
+            }
         }
     }
 
@@ -46,5 +68,11 @@ public class FreshMysql{
         if(longAdder != null){
             longAdder.decrement();
         }
+    }
+
+    @PreDestroy
+    public void destroy(){
+        log.info("程序关闭前写入mysql");
+        FreshCounter();
     }
 }
